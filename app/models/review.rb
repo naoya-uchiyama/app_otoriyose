458e1c8f2f2a4cdbb91d13afb7694f172095dad3
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-07-20 18:02:11 +0900

    commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,6 @@ class Review < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
-  has_many :comments, dependent: :destroy
   
   validates :title, :content, presence: true
   mount_uploader :image, ImageUploader
