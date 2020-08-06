440da41786dd9e7e9cbc940849bd5cd30d7cfcc4
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-08-07 05:17:53 +0900

    commit

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,90 +1,100 @@
-$(function(){
-  function buildHTML(review){
-    if(review.image.url !== null) {
-                  var html =  
-                  `<div class="review-container__box__content__title">
-                    ${review.title}
-                  </div>
-                  <div class="review-container__box__content__box">
-                  <div class="reviewer">
-                    ${review.user_nickname} : 
-                  </div>
-                  <div class="create-at">
-                    ${review.created_at}
-                  </div>
-                </div>
-                <div class="review-container__box__content__content">
-                  ${review.content}
-                </div>
-                <div class="review-container__box__content__image">
-                  <img src= "${review.image.url}">
-                </div>`
-                } else {
-                  var html =
-                  `<div class="review-container__box__content__title">
-                  ${review.title}
-                </div>
-                <div class="review-container__box__content__box">
-                  <div class="reviewer">
-                    ${review.user_nickname} : 
-                  </div>
-                  <div class="create-at">
-                    ${review.created_at}
-                  </div>
-                </div>
-                <div class="review-container__box__content__content">
-                  ${review.content}
-                </div>`
-                }
-    return html;
-  }
+// $(function(){
+//   function buildHTML(review){
+//     if(review.image.url !== null) {
+//                   var html =  
+//                   `<div class="review-container__box__content__title">
+//                     ${review.title}
+//                   </div>
+//                   <div class="review-conteiner__box__content__star">
+//                     <p>おいしさ</p>
+//                     <div class="star-delicious" id="star-delicious"></div>
+//                     <p>手軽さ</p>
+//                     <div class="star-eazy" id="star-eazy"></div>
+//                     <p>コストパフォーマンス</p>
+//                     <div class="star-cost" id="star-cost"></div>
+//                   </div>
+//                   <div class="review-container__box__content__box">
+//                   <div class="reviewer">
+//                     ${review.user_nickname} : 
+//                   </div>
+//                   <div class="create-at">
+//                     ${review.created_at}
+//                   </div>
+//                 </div>
+//                 <div class="review-container__box__content__content">
+//                   ${review.content}
+//                 </div>
+//                 <div class="review-container__box__content__image">
+//                   <img src= "${review.image.url}">
+//                 </div>`
+//                 } else {
+//                   var html =
+//                   `<div class="review-container__box__content__title">
+//                   ${review.title}
+//                 </div>
+//                 <div class="review-conteiner__box__content__star">
+//                   <p>おいしさ</p>
+//                   <div class="star-delicious" id="star-delicious"></div>
+//                   <p>手軽さ</p>
+//                   <div class="star-eazy" id="star-eazy"></div>
+//                   <p>コストパフォーマンス</p>
+//                   <div class="star-cost" id="star-cost"></div>
+//                 </div>
+//                 <div class="review-container__box__content__box">
+//                   <div class="reviewer">
+//                     ${review.user_nickname} : 
+//                   </div>
+//                   <div class="create-at">
+//                     ${review.created_at}
+//                   </div>
+//                 </div>
+//                 <div class="review-container__box__content__content">
+//                   ${review.content}
+//                 </div>`
+//                 }
+//     return html;
+//   }
 
-  var ReviewValid = {
-  rules: {
-    "review[title]": {
-      required: true,
-    },
-    "review[content]": {
-      required: true,
-    },
-  },
-  messages: {
-    "review[title]": {
-      required: 'レビュータイトルを入力してください',
-    },
-    "review[content]": {
-      required: 'レビューを入力してください',
-    },
-  }}
+//   var ReviewValid = {
+//   rules: {
+//     "review[title]": {
+//       required: true,
+//     },
+//   },
+//   messages: {
+//     "review[title]": {
+//       required: 'レビュータイトルを入力してください',
+//     },
+//   }}
   
 
-  $('#new-review').on("submit", function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $(this).validate(ReviewValid);
-    if (!$(this).valid()) {
-      return false;
-    };
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.review-container__box__content').prepend(html);
-      $('#review-title').val('');
-      $('#review-content').val('');
-      $('#review-image').val('');
-      $('#new-submit').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-});
+//   $('#new-review').on("submit", function(e){
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action');
+//     $(this).validate(ReviewValid);
+//     if (!$(this).valid()) {
+//       return false;
+//     };
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildHTML(data);
+//       $('.review-container__box__content').prepend(html);
+//       $('#review-title').val('');
+//       $('#review-content').val('');
+//       $('#review-image').val('');
+//       $('#new-submit').prop('disabled', false);
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+//   })
+// });
 
