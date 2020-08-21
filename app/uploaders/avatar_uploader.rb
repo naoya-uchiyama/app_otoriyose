9d7897dae93a4ce6cc710d5e5db0dda82be76cc2
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-08-22 00:20:06 +0900

    commit

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,14 +5,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [200, 200] 
   # Choose what kind of storage to use for this uploader:
 
-  # if Rails.env.development?
-  #   storage :file
-  # elsif Rails.env.test?
-  #   storage :file
-  # else
-  #   storage :fog
-  # end
-  storage :file
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
