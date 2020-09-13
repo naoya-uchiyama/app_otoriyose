671cf0269a0cff17ab6a75fa83d00054fd2daf58
naoya-uchiyama <naoya.uchiyama55@gmail.com>
2020-09-13 12:16:56 +0900

    setting

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,6 +1,6 @@
 RakutenWebService.configure do |c|
-  c.application_id = ENV['RWS_APPLICATION_ID']
+  c.application_id = Rails.application.credentials[:RWS_APPLICATION_ID]
 
-  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
+  c.affiliate_id = Rails.application.credentials[:RWS_AFFILIATION_ID]
 
 end
